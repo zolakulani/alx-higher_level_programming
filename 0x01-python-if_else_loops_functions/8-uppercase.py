5738be5798dde13ddbc6df915f691ba08236af51
zolakulani <shikwenizolakulani@gmail.com>
2024-01-03 09:25:58 -0800

    prints a string in uppercase followed by a new line

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+def uppercase(str):
+    for up in str:
+        temp = up
+        if ord(temp) >= 97 and ord(temp) <= 122:
+            temp = chr(ord(up) - 32)
+        print("{}".format(temp), end="")
+    print()
