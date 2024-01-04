40783b1369010e1512ecd8773337af51bbf59472
zolakulani <shikwenizolakulani@gmail.com>
2024-01-04 10:52:15 -0800

    Print all names defined by hidden_4 module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,14 +2,15 @@
 
 if __name__ == "__main__":
     """Print all names defined by hidden_4 module."""
-    
+
     """ Import all names from the hidden_4 module """
     from hidden_4 import *
 
-    """ Get all names defined in the current  scope"""
+    """ Get all names defined in the current scope """
     all_names = dir()
 
-    """Iterate through the names and print those not starting with "__" """
+    """ Iterate through the names and print those not starting with "__" """
     for i in range(len(all_names)):
         if all_names[i][:2] != "__":
+            """ Print the name """
             print("{:s}".format(all_names[i]))
