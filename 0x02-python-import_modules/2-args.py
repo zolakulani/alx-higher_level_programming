eed4c55cfdf6e5053b48690ba3ac52c412099ef8
zolakulani <shikwenizolakulani@gmail.com>
2024-01-04 08:47:50 -0800

    prints the number of and the list of its arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
+
+# Check if the script is being run as the main program
 if __name__ == "__main__":
+    # Import the argv list from the sys module
     from sys import argv
+
+    # Calculate the number of arguments
     x = len(argv) - 1
+
+    # Check the number of arguments and print accordingly
     if x < 1:
         print("{} arguments:".format(x))
     elif x == 1:
         print("{} argument.".format(x))
     else:
         print("{} arguments:".format(x))
+
+    # Print the position and value of each argument
     for i in range(x):
         print("{}: {:s}".format(i + 1, argv[i + 1]))
