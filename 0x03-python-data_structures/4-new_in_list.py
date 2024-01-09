13098111667f7d0913eaa21c6f81ad7b0e7a65eb
zolakulani <shikwenizolakulani@gmail.com>
2024-01-09 11:41:43 -0800

    main files

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,15 +1,4 @@
 #!/usr/bin/python3
-"""
-    Replaces the element at the specified index in a given list with a new element.
-
-    Parameters:
-        - my_list (list): The original list.
-        - idx (int): The index at which the element needs to be replaced.
-        - element: The new element to be inserted at the specified index.
-
-    Returns:
-        list: A new list with the element at the specified index replaced. If the index is out of range, the original list is returned.
-"""
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
