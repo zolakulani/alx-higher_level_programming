a3dbd34312a63112162100ae2bb28ab22adb4453
zolakulani <shikwenizolakulani@gmail.com>
2024-01-09 15:07:03 +0200

    replace_in_list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,4 +3,6 @@ def replace_in_list(my_list, idx, element):
     for inx in range(len(my_list)):
         if inx == idx:
             my_list[inx] = element
+        if inx > idx:
+            break
     return my_list
