c652a0e7832774540f5522e1e9e4cfc00cac8185
zolakulani <shikwenizolakulani@gmail.com>
2024-01-09 11:47:06 -0800

    main files

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,15 +1,4 @@
 #!/usr/bin/python3
-"""
-Replaces an element at a specific index in a list.
-
-Parameters:
-my_list (list): The list in which the element should be replaced.
-idx (int): The index of the element to be replaced.
-element: The element to replace the existing element at the specified index.
-
-Returns:
-list: The modified list with the element replaced at the specified index.
-"""
 def replace_in_list(my_list, idx, element):
     for inx in range(len(my_list)):
         if inx == idx:
@@ -17,4 +6,3 @@ def replace_in_list(my_list, idx, element):
         if inx > idx:
             break
     return my_list
-              
