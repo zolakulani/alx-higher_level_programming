18f66646298095c44bdf42e027c9908cad57f6c9
zolakulani <shikwenizolakulani@gmail.com>
2024-01-09 16:05:18 +0200

    replace_in_list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,8 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     for inx in range(len(my_list)):
         if inx == idx:
             my_list[inx] = element
-            return my_list
         if inx > idx:
-            return my_list
+            break
     return my_list
