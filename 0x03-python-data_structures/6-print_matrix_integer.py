1d044bbae83b3df6118cd0795904a98afa611a49
zolakulani <shikwenizolakulani@gmail.com>
2024-01-09 11:19:12 -0800

    main files

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,5 @@ def print_matrix_integer(matrix=[[]]):
     if matrix:
         for row in matrix:
             for col in row:
-                print("{:di}".format(col), end=" " if col != row[-1] else '')
+                print("{:d}".format(col), end=' ' if col != row[-1] else '')
             print("")
-    return
