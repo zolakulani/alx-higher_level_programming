45c825ef4bdc13bfbda23b79b1db203852a4a3a6
zolakulani <shikwenizolakulani@gmail.com>
2024-05-23 12:25:39 -0700

    whole production

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,1 +1,16 @@
--- 
+-- Whole creation
+-- 9-full_creation.sql
+
+-- Create the table second_table if it does not exist
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+-- Insert multiple rows into second_table
+INSERT INTO second_table (id, name, score) VALUES 
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
