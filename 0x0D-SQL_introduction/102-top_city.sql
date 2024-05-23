24e2ded951a38d33d958813539ef2ccc79933a7a
zolakulani <shikwenizolakulani@gmail.com>
2024-05-23 13:01:28 -0700

    python

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,3 @@
+-- list the top three cities.
+-- Execute: cat 102-top_city.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month IN (7, 8) GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
